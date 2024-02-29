f33e54a3b682f4e2e405c0235cbc6d4967def7cb
PerchB0i <areksiut@gmail.com>
2024-02-29 15:36:40 +0100

    ex. 1

--- src/Segment.java
+++ src/Segment.java
@@ -15,7 +15,7 @@ public class Segment {
     }
 
     public double getDistance(){
-        //return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+        // return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
         return Math.hypot(p1.x - p2.x, p1.y - p2.y);
     }
 }
