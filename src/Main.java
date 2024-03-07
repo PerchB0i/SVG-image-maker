6415e23eaeffe5015cb787717d44994f48c64fc2
PerchB0i <areksiut@gmail.com>
2024-03-07 16:53:36 +0100

    added SvgScene class with writing to HTML file

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,21 @@
 public class Main {
 
     public static void main(String[] args) {
-        Style style = new Style(null, null ,null);
+        Style style = new Style("Lime", "red" ,2.0);
         System.out.println(style.toSVG());
+
+        Point point1 = new Point(100, 0);
+        Point point2 = new Point(50, 100);
+        Point point3 = new Point(150, 100);
+
+        Polygon polygon = new Polygon(3, style);
+        polygon.setPoint(0, point1);
+        polygon.setPoint(1, point2);
+        polygon.setPoint(2, point3);
+
+        SvgScene scene = new SvgScene();
+        scene.addPolygon(polygon);
+        scene.saveHTML("file.html");
     }
 
 }
